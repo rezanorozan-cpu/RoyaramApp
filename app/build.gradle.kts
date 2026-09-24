41dcfe5ce61e80eeda6262707a7b492c72363d4e
rezanorozan-cpu <rezanorozan@gmail.com>
2026-09-25 03:25:41 +0330

    Update Kotlin options to use new DSL syntax

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,9 +25,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+}
 
-    kotlinOptions {
-        jvmTarget = "17"
+kotlin {
+    compilerOptions {
+        jvmTarget.set(
+            org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        )
     }
 }
 
