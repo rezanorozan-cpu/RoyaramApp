eb732889317d4b506bc93628232a87a571272daf
rezanorozan-cpu <rezanorozan@gmail.com>
2026-09-24 18:24:28 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,8 @@ android {
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2024.10.01")
+ implementation("io.coil-kt:coil-compose:2.7.0")  
+val composeBom = platform("androidx.compose:compose-bom:2024.10.01")
 
     implementation(composeBom)
     implementation("androidx.activity:activity-compose:1.9.3")
