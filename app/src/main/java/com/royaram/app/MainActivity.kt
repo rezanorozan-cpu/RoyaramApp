0e68ba470f816c13d4709a9191dbbe6195f7f97d
rezanorozan-cpu <rezanorozan@gmail.com>
2026-09-26 03:25:34 +0330

    Update MainActivity.kt

--- app/src/main/java/com/royaram/app/MainActivity.kt
+++ app/src/main/java/com/royaram/app/MainActivity.kt
@@ -57,6 +57,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
