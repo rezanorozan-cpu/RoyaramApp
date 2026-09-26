44be2b5dbbd764783b9a9320e0f1554586f5a7ab
rezanorozan-cpu <rezanorozan@gmail.com>
2026-09-26 04:14:02 +0330

    Add Firebase Storage dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,5 +55,6 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.19.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage")
     implementation("androidx.biometric:biometric:1.1.0")
 }
